32c4c4856a323727b48edcd2edac2f94eac22bbe
hemanthkumar316 <hemanth118818@gmail.com>
2022-02-14 12:52:44 +0530

    ok

--- react_learn/Backup-practice/Hooks-useState/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-class Navbar extends React.Component{
-    render(){
-        return<>
-        <nav className="navbar navbar-dark bg-dark text-white">React Hooks-useState</nav>
-        </>
-    }
-}
-export default Navbar
