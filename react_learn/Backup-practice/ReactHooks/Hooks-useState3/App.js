d3a898944b2c6497e8ad9f115be763c3509c93c8
hemanthkumar316 <hemanth118818@gmail.com>
2022-02-19 12:44:40 +0530

    ok

--- react_learn/Backup-practice/ReactHooks/Hooks-useState3/App.js
+++ react_learn/Backup-practice/ReactHooks/Hooks-useState3/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Hike from './Hike'
+import Bind from'./Bind'
 const App= () => {
   return (
     <div>
     <Hike/>
+    <hr/>
+    <Bind/>
     </div>
   )
 }
--- react_learn/src/index.js
+++ react_learn/src/index.js
@@ -1,4 +1,4 @@
 import ReactDOM from'react-dom'
-import App from '../Backup-practice/ReactHooks/Hooks-useState3/App'
+import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 ReactDOM.render(<App/>,document.getElementById('root'))
